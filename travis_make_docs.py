41b1783c2df98c2507f46e5610376656c0e42188
mzwiessele <ibinbei@gmail.com>
2017-02-21 16:14:24 +0000

    fix: make docs through python

--- travis_make_docs.py
+++ travis_make_docs.py
@@ -22,7 +22,7 @@ def create_meta_header(folder):
     _add_meta(meta, 'description', meta_objects)
 
     import time
-    meta['date'] = time.strftime("%Y-%m-%d %H:%M:%S %z")
+    meta.setdefault('date', time.strftime("%Y-%m-%d %H:%M:%S %z"))
     _add_meta(meta, 'date', meta_objects)
     
     _add_meta(meta, 'keywords', meta_objects, meta_key='categories')
@@ -40,33 +40,39 @@ def create_meta_header(folder):
     
     return '\n'.join(meta_objects), time.strftime("%Y-%m-%d-{}".format('-'.join(meta['title'].split(' ')))), meta
 
+def create_docs_filename(title):
+    # Thanks to Django framework: https://docs.djangoproject.com
+    import re
+    title = re.sub('[^\w\s-]', '', title).strip().lower()
+    title = re.sub('[-\s]+', '-', title)
+    return title
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    import nbformat, os
+    import sys, nbformat, os, subprocess
     from nbconvert.preprocessors import ExecutePreprocessor
     from nbconvert.preprocessors.execute import CellExecutionError
     from nbconvert.exporters import MarkdownExporter
 
+    argv = sys.argv[1:]
     note_folder = argv[0]
 
     header, filename, meta_data = create_meta_header(note_folder) # Generate metadata header
     # Make blog post with metadata header
-
+    filename = create_docs_filename(meta_data['title'])
     import codecs
     outfolder='_under_review'
-    if meta_data['accepted']:
-        reviewpath = 'docs/{}/{}.md'.format(out_folder, meta_data['title'])
+
+    if meta_data['accepted'] in ['true', 'yes', 'y']:
+        reviewpath = 'docs/{}/{}.md'.format(outfolder, filename)
         if os.path.exists(reviewpath):
-            os.remove(reviewpath)
+            subprocess.call(["git", "rm", reviewpath])
         outfolder='_accepted' # put the new file into accepted
-    outpath = 'docs/{}/{}.md'.format(out_folder, meta_data['title'])
+    outpath = 'docs/{}/{}.md'.format(outfolder, filename)
     with codecs.open(outpath, 'w', 'utf-8') as f:
         # Write a header for the gh-pages website and safe it for later usage
         f.seek(0)
         f.write(header)
         f.write('\n')
 
-    import subprocess
-    subprocess.call(["git", "add", reviewpath, outpath])
+    subprocess.call(["git", "add", outpath])
     
